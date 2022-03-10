678a2d994c525cd281ae8a71f5af72913a184979
wangfei9527 <101188639+wangfei9527@users.noreply.github.com>
2022-03-10 10:39:21 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     let targetImage = document.querySelector("#smart-image");
 targetImage.addEventListener("click", function() {
-    if (targetImage.classList.contains("small")) {
+    targetImage.classList.add("small");
     targetImage.classList.remove("small");
-} else {
-   targetImage.classList.add("small");
-}
+
 });
 });
